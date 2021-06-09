6f2f59fd3967444cf7b1ea8b8a6c6c7db25eca23
Sergiod3x <sergiod3x.@gmail.com>
2021-06-09 13:53:28 +0200

    pari ok

--- js/script.js
+++ js/script.js
@@ -22,5 +22,37 @@ if(palindromo(word)){
 }
     
 
+var num = parseInt( prompt("Ora giochiamo a pari o dispari, scegli un numero da 1 a 5"));
 
 
+while(num > 5 || Number.isNaN(num)){
+    num = parseInt(prompt("Attento, scegli un numero da 1 a 5"));
+}
+
+var pDWord =  prompt("Scegli pari o dispari");
+
+if (pDWord === "pari"||pDWord === "Pari"|| pDWord === "P"|| pDWord === "p"){
+    var pari = 0;
+} else {
+    var pari = 1;
+}
+
+function getRandomNumber(){
+    return (Math.floor(Math.random() * 5)+1);
+}
+
+var p2Num = getRandomNumber();
+var total = num + p2Num;
+
+function winner(a, c){
+    if (a%2 == c) {
+        output.innerText += "\n Hai vinto!!" ;
+    } else {
+            output.innerText += "\n Ha vinto il pc!!" ;
+    }
+}
+
+winner(total, pari);
+
+// output.innerText += "\n"+  total + "\n" + pari+ "\n" +p2Num + "\n" +num  ;
+     
